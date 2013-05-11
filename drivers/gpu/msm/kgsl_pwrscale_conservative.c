1a9753a75ca61c6d1f78c7a917fd872365d572f5
myfluxi <linflux@arcor.de>
2013-05-11 12:58:04 +0200

    msm: kgsl: conservative: Lindent, cleanup, adjust thresholds

--- drivers/gpu/msm/kgsl_pwrscale_conservative.c
+++ drivers/gpu/msm/kgsl_pwrscale_conservative.c
@@ -23,10 +23,10 @@
 #include "kgsl_pwrscale.h"
 #include "kgsl_device.h"
 
-// 1 = print statistics
+/* 1 = print statistics */
 static unsigned int g_show_stats = 0;
 
-// Polling interval in us
+/* Polling interval in us */
 #define MIN_POLL_INTERVAL 10000
 #define MAX_POLL_INTERVAL 1000000
 static unsigned long g_polling_interval = 100000;
@@ -34,9 +34,9 @@ static unsigned long g_polling_interval = 100000;
 static unsigned long walltime_total = 0;
 static unsigned long busytime_total = 0;
 
-struct gpu_thresh_tbl{
-    const unsigned int up_threshold;
-    const unsigned int down_threshold;
+struct gpu_thresh_tbl {
+	const unsigned int up_threshold;
+	const unsigned int down_threshold;
 };
 
 #define GPU_SCALE(u,d) \
@@ -46,192 +46,187 @@ struct gpu_thresh_tbl{
     }
 
 static struct gpu_thresh_tbl thresh_tbl[] = {
-    GPU_SCALE(110,60),
-    GPU_SCALE(90,45),
-    GPU_SCALE(80,45),
-    GPU_SCALE(80,0),
-    GPU_SCALE(100,0),
+	GPU_SCALE(110, 60),
+	GPU_SCALE(90, 45),
+	GPU_SCALE(75, 35),
+	GPU_SCALE(60, 0),
+	GPU_SCALE(100, 0),
 };
 
-static void conservative_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
+static void conservative_wake(struct kgsl_device *device,
+			      struct kgsl_pwrscale *pwrscale)
 {
-    struct kgsl_power_stats stats;
+	struct kgsl_power_stats stats;
 
-    if (g_show_stats == 1)
-        pr_info("%s: GPU waking up\n",__func__);
+	if (g_show_stats == 1)
+		pr_info("%s: GPU waking up\n", KGSL_NAME);
 
-    if (device->state != KGSL_STATE_NAP) {
-        kgsl_pwrctrl_pwrlevel_change(device,
-                    device->pwrctrl.default_pwrlevel);
-        //reset the power stats counters;
-        device->ftbl->power_stats(device,&stats);
-        walltime_total = 0;
-        busytime_total = 0;
-    }
+	if (device->state != KGSL_STATE_NAP) {
+		kgsl_pwrctrl_pwrlevel_change(device,
+					     device->pwrctrl.default_pwrlevel);
+		/* reset the power stats counters */
+		device->ftbl->power_stats(device, &stats);
+		walltime_total = 0;
+		busytime_total = 0;
+	}
 }
 
-static void conservative_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
+static void conservative_idle(struct kgsl_device *device,
+			      struct kgsl_pwrscale *pwrscale)
 {
-    struct kgsl_pwrctrl *pwr = &device->pwrctrl;
-    struct kgsl_power_stats stats;
-    int val = 0;
-    unsigned int loadpct;
+	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
+	struct kgsl_power_stats stats;
+	int val = 0;
+	unsigned int loadpct;
 
-    device->ftbl->power_stats(device, &stats);
+	device->ftbl->power_stats(device, &stats);
 
-    if (stats.total_time == 0)
-        return;
+	if (stats.total_time == 0)
+		return;
 
-    walltime_total += (unsigned long) stats.total_time;
-    busytime_total += (unsigned long) stats.busy_time;
-    if (walltime_total > g_polling_interval)
-    {
-        if (g_show_stats == 1)
-            pr_info("%s: walltime_total = %lu, busytime_total = %lu\n",__func__,walltime_total,busytime_total);
+	walltime_total += (unsigned long)stats.total_time;
+	busytime_total += (unsigned long)stats.busy_time;
 
-        loadpct = (100 * busytime_total) / walltime_total;
+	if (walltime_total <= g_polling_interval)
+		return;
 
-        if (g_show_stats == 1)
-            pr_info("%s: loadpct = %d\n",__func__,loadpct);
+	if (g_show_stats == 1)
+		pr_info("%s: walltime_total: %lu, busytime_total: %lu\n",
+			KGSL_NAME, walltime_total, busytime_total);
 
-        walltime_total = busytime_total = 0;
+	loadpct = (100 * busytime_total) / walltime_total;
 
-        if(loadpct < thresh_tbl[pwr->active_pwrlevel].down_threshold)
-            val = 1;
-        else if(loadpct > thresh_tbl[pwr->active_pwrlevel].up_threshold)
-            val = -1;
+	walltime_total = busytime_total = 0;
 
-        if (g_show_stats == 1)
-            pr_info("%s: active_pwrlevel = %d, change = %d\n",__func__,pwr->active_pwrlevel,val);
+	if (loadpct < thresh_tbl[pwr->active_pwrlevel].down_threshold)
+		val = 1;
+	else if (loadpct > thresh_tbl[pwr->active_pwrlevel].up_threshold)
+		val = -1;
 
-        if (val)
-            kgsl_pwrctrl_pwrlevel_change(device,
-                       pwr->active_pwrlevel + val);
-    }
+	if (g_show_stats == 1)
+		pr_info("%s: loadpct: %d, active_pwrlevel: %d, change: %d\n",
+			KGSL_NAME, loadpct, pwr->active_pwrlevel, val);
+
+	if (val)
+		kgsl_pwrctrl_pwrlevel_change(device,
+					     pwr->active_pwrlevel + val);
 }
 
 static void conservative_busy(struct kgsl_device *device,
-                    struct kgsl_pwrscale *pwrscale)
+			      struct kgsl_pwrscale *pwrscale)
 {
-    device->on_time = ktime_to_us(ktime_get());
+	device->on_time = ktime_to_us(ktime_get());
 }
 
 static void conservative_sleep(struct kgsl_device *device,
-                    struct kgsl_pwrscale *pwrscale)
+			       struct kgsl_pwrscale *pwrscale)
 {
-    if (g_show_stats == 1)
-        pr_info("%s: GPU going to sleep\n",__func__);
+	if (g_show_stats == 1)
+		pr_info("%s: GPU going to sleep\n", KGSL_NAME);
 }
 
-static ssize_t conservative_stats_show(
-                    struct kgsl_device *device,
-		    struct kgsl_pwrscale *pwrscale,
-                    char *buf)
+static ssize_t conservative_stats_show(struct kgsl_device *device,
+				       struct kgsl_pwrscale *pwrscale,
+				       char *buf)
 {
-    int count;
+	pr_info("%s: Print statistics: %u\n", KGSL_NAME, g_show_stats);
 
-    count = sprintf(buf, "%u\n", g_show_stats);
-    pr_info("%s: Print statistics = %u\n", __func__, g_show_stats);
-
-    return count;
+	return sprintf(buf, "%u\n", g_show_stats);
 }
 
-static ssize_t conservative_stats_store(
-                    struct kgsl_device *device,
-		    struct kgsl_pwrscale *pwrscale,
-                    const char *buf,
-                    size_t count)
+static ssize_t conservative_stats_store(struct kgsl_device *device,
+					struct kgsl_pwrscale *pwrscale,
+					const char *buf, size_t count)
 {
-    unsigned int tmp;
-    int err;
+	unsigned int tmp;
+	int err;
 
-    err = kstrtoint(buf, 0, &tmp);
-    if (err) {
-        pr_err("%s: failed setting stats show!\n", __func__);
-        return err;
-    }
+	err = kstrtoint(buf, 0, &tmp);
+	if (err) {
+		pr_err("%s: failed setting stats show!\n", KGSL_NAME);
+		return err;
+	}
 
-    g_show_stats = tmp ? 1 : 0;
+	g_show_stats = tmp ? 1 : 0;
 
-    pr_info("%s: Print statistics = %u\n", __func__, g_show_stats);
+	pr_info("%s: Print statistics: %u\n", KGSL_NAME, g_show_stats);
 
-    return count;
+	return count;
 }
 
-PWRSCALE_POLICY_ATTR(print_stats, 0666, conservative_stats_show, conservative_stats_store);
+PWRSCALE_POLICY_ATTR(print_stats, 0644, conservative_stats_show,
+		     conservative_stats_store);
 
-static ssize_t conservative_polling_interval_show(
-                    struct kgsl_device *device,
-		    struct kgsl_pwrscale *pwrscale,
-                    char *buf)
+static ssize_t conservative_polling_interval_show(struct kgsl_device *device, struct kgsl_pwrscale
+						  *pwrscale, char *buf)
 {
-    int count;
-
-    count = sprintf(buf, "%lu\n", g_polling_interval);
-    pr_info("%s: polling interval = %lu\n", __func__, g_polling_interval);
-
-    return count;
+	return sprintf(buf, "%lu\n", g_polling_interval);
 }
 
-static ssize_t conservative_polling_interval_store(
-                    struct kgsl_device *device,
-		    struct kgsl_pwrscale *pwrscale,
-                    const char *buf,
-                    size_t count)
+static ssize_t conservative_polling_interval_store(struct kgsl_device *device, struct kgsl_pwrscale
+						   *pwrscale, const char *buf,
+						   size_t count)
 {
-    unsigned long tmp;
-    int err;
+	unsigned long tmp;
+	int err;
 
-    err = kstrtoul(buf, 0, &tmp);
-    if (err) {
-        pr_err("%s: failed setting new polling interval!\n", __func__);
-        return err;
-    }
+	err = kstrtoul(buf, 0, &tmp);
+	if (err) {
+		pr_err("%s: failed setting new polling interval!\n", KGSL_NAME);
+		return err;
+	}
 
-    if (tmp < MIN_POLL_INTERVAL)
-        tmp = MIN_POLL_INTERVAL;
-    else if (tmp > MAX_POLL_INTERVAL)
-        tmp = MAX_POLL_INTERVAL;
+	if (tmp < MIN_POLL_INTERVAL)
+		tmp = MIN_POLL_INTERVAL;
+	else if (tmp > MAX_POLL_INTERVAL)
+		tmp = MAX_POLL_INTERVAL;
 
-    g_polling_interval = tmp;
+	g_polling_interval = tmp;
 
-    pr_info("%s: new polling interval = %lu\n", __func__, g_polling_interval);
+	if (g_show_stats == 1)
+		pr_info("%s: new polling interval: %lu\n", KGSL_NAME,
+			g_polling_interval);
 
-    return count;
+	return count;
 }
 
-PWRSCALE_POLICY_ATTR(polling_interval, 0666, conservative_polling_interval_show, conservative_polling_interval_store);
+PWRSCALE_POLICY_ATTR(polling_interval, 0644, conservative_polling_interval_show,
+		     conservative_polling_interval_store);
 
 static struct attribute *conservative_attrs[] = {
-    &policy_attr_print_stats.attr,
-    &policy_attr_polling_interval.attr,
-    NULL
+	&policy_attr_print_stats.attr,
+	&policy_attr_polling_interval.attr,
+	NULL
 };
 
 static struct attribute_group conservative_attr_group = {
-    .attrs = conservative_attrs,
-    .name = "conservative",
+	.attrs = conservative_attrs,
 };
 
-static int conservative_init(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
+static int conservative_init(struct kgsl_device *device,
+			     struct kgsl_pwrscale *pwrscale)
 {
-    kgsl_pwrscale_policy_add_files(device, pwrscale, &conservative_attr_group);
+	kgsl_pwrscale_policy_add_files(device, pwrscale,
+				       &conservative_attr_group);
 
-    return 0;
+	return 0;
 }
 
-static void conservative_close(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
+static void conservative_close(struct kgsl_device *device,
+			       struct kgsl_pwrscale *pwrscale)
 {
-    kgsl_pwrscale_policy_remove_files(device, pwrscale, &conservative_attr_group);
+	kgsl_pwrscale_policy_remove_files(device, pwrscale,
+					  &conservative_attr_group);
 }
 
 struct kgsl_pwrscale_policy kgsl_pwrscale_policy_conservative = {
-    .name = "conservative",
-    .init = conservative_init,
-    .busy = conservative_busy,
-    .idle = conservative_idle,
-    .sleep = conservative_sleep,
-    .wake = conservative_wake,
-    .close = conservative_close
+	.name = "conservative",
+	.init = conservative_init,
+	.busy = conservative_busy,
+	.idle = conservative_idle,
+	.sleep = conservative_sleep,
+	.wake = conservative_wake,
+	.close = conservative_close
 };
+
 EXPORT_SYMBOL(kgsl_pwrscale_policy_conservative);
